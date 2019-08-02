f594e6270554266940a083a6ca904158dc0f9b14
王宇 <18766267141@163.com>
2019-08-02 16:52:13 +0800

    test commit

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -27,7 +27,7 @@ public class StudentController {
 
     @RequestMapping(value = "/qryByIds")
     public Student qrys(int id){
-        System.out.println("修改123");
+        System.out.println("修改1232222");
         return service.queryById(id);
     }
 }
